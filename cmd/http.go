590e09e9245b208fa7315d8b6caf4fc8cc2c2876
Adam Clark <aclark@walmartlabs.com>
2014-11-14 01:09:18 -0700

    writing body to tmp folder

--- cmd/http.go
+++ cmd/http.go
@@ -3,14 +3,17 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	nethttp "net/http"
 	"net/url"
 	"os"
+	"os/user"
 	"strings"
 	"time"
 
 	"github.com/adamclerk/mario/lib"
 	"github.com/codegangsta/cli"
+	"github.com/twinj/uuid"
 )
 
 type httpFactory struct{}
@@ -20,11 +23,18 @@ type http struct {
 	request      *nethttp.Request
 	err          error
 	responseTime time.Duration
+	tmpdir       string
+	line         string
+	file         string
 	writer       io.Writer
 }
 
 func (f *httpFactory) Make(line string, context *cli.Context) lib.Task {
-	values := strings.Split(line, context.String("separator"))
+	return MakeHTTP(line, context.String("separator"), context.String("tmpdir"))
+}
+
+func MakeHTTP(line string, separator string, tmpdir string) *http {
+	values := strings.Split(line, separator)
 
 	request := nethttp.Request{}
 	url, err := url.Parse(values[0])
@@ -53,12 +63,15 @@ func (f *httpFactory) Make(line string, context *cli.Context) lib.Task {
 	return &http{
 		request: &request,
 		writer:  os.Stdout,
+		tmpdir:  tmpdir,
+		line:    line,
 		err:     nil,
 	}
 }
 
 func (h *http) Print() {
-	fmt.Fprintf(h.writer, "%d,%s,%d\n", h.response.StatusCode, h.responseTime, h.responseTime)
+
+	fmt.Fprintf(h.writer, "%s,%d,%d,%s\n", h.line, h.response.StatusCode, h.responseTime/1000000, h.file)
 }
 
 func (h *http) Process() {
@@ -75,9 +88,24 @@ func (h *http) Process() {
 	}
 	h.responseTime = time.Since(time_start)
 	h.response = response
+
+	usr, _ := user.Current()
+	dirErr := os.MkdirAll(usr.HomeDir+"/"+h.tmpdir+"/http", 0755)
+	h.file = usr.HomeDir + "/" + h.tmpdir + "/http/" + uuid.Formatter(uuid.NewV4(), uuid.Clean) + ".body"
+	defer h.response.Body.Close()
+	body, err := ioutil.ReadAll(h.response.Body)
+	if dirErr != nil {
+		body = []byte("error")
+	}
+	writeErr := ioutil.WriteFile(h.file, body, 0664)
+	if writeErr != nil {
+		panic(writeErr)
+		h.file = "write error"
+	}
 }
 
 func AddHTTP() cli.Command {
+
 	return cli.Command{
 		Name:      "http",
 		ShortName: "w",
@@ -91,6 +119,11 @@ func AddHTTP() cli.Command {
 				Name:  "separator",
 				Value: ",",
 				Usage: "separator character",
+			},
+			cli.StringFlag{
+				Name:  "tmpdir",
+				Value: ".mario",
+				Usage: "temp directory where we store the body",
 			},
 		},
 	}
--- cmd/http_test.go
+++ cmd/http_test.go
@@ -3,35 +3,47 @@ package cmd
 import (
 	"io"
 	"io/ioutil"
+	"strings"
 	"testing"
 )
 
-type httpProcessTest struct {
-	line     string
-	expected string
+type httpMakeTest struct {
+	line    string
+	method  string
+	url     string
+	headers string
 }
 
-var httpProcessTestCases = []httpProcessTest{
-/*{"one,two,three", ""},
-  {"two,three,four", ""},
-  {"club,breakfast", ""},*/
+var httpMakeTestCases = []httpMakeTest{
+	{"http://www.google.com", "GET", "http://www.google.com", ""},
+	{"http://www.facebook.com,POST", "POST", "http://www.facebook.com", ""},
+	{"http://www.twitter.com,PUT,accept: text/html", "PUT", "http://www.twitter.com", "Accept: text/html"},
+}
+
+func TestHttpMake(t *testing.T) {
+	for _, test := range httpMakeTestCases {
+		http := MakeHTTP(test.line, ",", ".mariotest")
+		if test.url != http.request.URL.String() {
+			t.Error("Expect '"+test.url, "'"+http.request.URL.String()+"'")
+		}
+
+		if test.method != http.request.Method {
+			t.Error("Expect '"+test.method, "'"+http.request.Method+"'")
+		}
+
+		r, w := io.Pipe()
+		go func() {
+			http.request.Header.Write(w)
+			w.Close()
+		}()
+		result, _ := ioutil.ReadAll(r)
+
+		if test.headers != strings.TrimSpace(string(result)) {
+			t.Error("Expect '"+test.headers, "'"+strings.TrimSpace(string(result))+"'")
+		}
+	}
 }
 
 func TestHttpPrint(t *testing.T) {}
 
-func TestHttpMake(t *testing.T) {}
-
-func TestHTTPProcess(t *testing.T) {
-	for _, test := range httpProcessTestCases {
-		r, w := io.Pipe()
-		go func() {
-			cmd := http{writer: w}
-			cmd.Print()
-			w.Close()
-		}()
-		result, _ := ioutil.ReadAll(r)
-		if test.expected+"\n" != string(result) {
-			t.Error("Expect '"+test.line+"\n'", "'"+string(result)+"'")
-		}
-	}
-}
+func TestHTTPProcess(t *testing.T) {}
